d730d59c1d7c4364e18bc1b79db7824347a69cec
eagudio <edoardoagudio@gmail.com>
2025-03-13 18:42:38 +0100

    chore: update package.json and tsconfig.json for new features
    - Bump version to 0.1.1 in package.json
    - Add exports field in package.json for better module resolution
    - Enable declaration files in tsconfig.json for TypeScript
    - Introduce src/html/index.ts to export basic HTML elements

---
+++ src/html/index.ts
@@ -0,0 +1,118 @@
+// Basic elements
+export { A } from './a';
+export { B } from './b';
+export { Br } from './br';
+export { Div } from './div';
+export { Hr } from './hr';
+export { I } from './i';
+export { P } from './p';
+export { Span } from './span';
+export { U } from './u';
+
+// Headings
+export { H1 } from './h1';
+export { H2 } from './h2';
+export { H3 } from './h3';
+export { H4 } from './h4';
+export { H5 } from './h5';
+export { H6 } from './h6';
+
+// Form elements
+export { Button } from './button';
+export { Datalist } from './datalist';
+export { Fieldset } from './fieldset';
+export { Form } from './form';
+export { Input } from './input';
+export { Label } from './label';
+export { Legend } from './legend';
+export { Meter } from './meter';
+export { Optgroup } from './optgroup';
+export { Option } from './option';
+export { Output } from './output';
+export { Progress } from './progress';
+export { Select } from './select';
+export { Textarea } from './textarea';
+
+// Lists
+export { Dl } from './dl';
+export { Dt } from './dt';
+export { Dd } from './dd';
+export { Li } from './li';
+export { Ol } from './ol';
+export { Ul } from './ul';
+
+// Tables
+export { Caption } from './caption';
+export { Col } from './col';
+export { Colgroup } from './colgroup';
+export { Table } from './table';
+export { Tbody } from './tbody';
+export { Td } from './td';
+export { Tfoot } from './tfoot';
+export { Th } from './th';
+export { Thead } from './thead';
+export { Tr } from './tr';
+
+// Semantic elements
+export { Article } from './article';
+export { Aside } from './aside';
+export { Dialog } from './dialog';
+export { Details } from './details';
+export { Figcaption } from './figcaption';
+export { Figure } from './figure';
+export { Footer } from './footer';
+export { Header } from './header';
+export { Main } from './main';
+export { Mark } from './mark';
+export { Nav } from './nav';
+export { Section } from './section';
+export { Summary } from './summary';
+
+// Media elements
+export { Audio } from './audio';
+export { Canvas } from './canvas';
+export { Embed } from './embed';
+export { Iframe } from './iframe';
+export { Img } from './img';
+export { Map } from './map';
+export { Object } from './object';
+export { Picture } from './picture';
+export { Source } from './source';
+export { Track } from './track';
+export { Video } from './video';
+
+// Text elements
+export { Bdi } from './bdi';
+export { Bdo } from './bdo';
+export { Blockquote } from './blockquote';
+export { Cite } from './cite';
+export { Code } from './code';
+export { Data } from './data';
+export { Del } from './del';
+export { Dfn } from './dfn';
+export { Em } from './em';
+export { Ins } from './ins';
+export { Kbd } from './kbd';
+export { Pre } from './pre';
+export { Q } from './q';
+export { Rp } from './rp';
+export { Rt } from './rt';
+export { Ruby } from './ruby';
+export { S } from './s';
+export { Samp } from './samp';
+export { Small } from './small';
+export { Strong } from './strong';
+export { Sub } from './sub';
+export { Sup } from './sup';
+export { Time } from './time';
+export { Var } from './var';
+export { Wbr } from './wbr';
+
+// Other elements
+export { Base } from './base';
+export { Meta } from './meta';
+export { Noscript } from './noscript';
+export { Script } from './script';
+export { Style } from './style';
+export { Template } from './template';
+export { Title } from './title'; 
